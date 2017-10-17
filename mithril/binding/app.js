0f004f8ff207936a195a2808f25979ef16c96eae
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2017-10-17 09:38:08 +0900

    mithrilのバインディングを試した

---
+++ mithril/binding/app.js
@@ -0,0 +1,36 @@
+// form.xと、mdl.mmbが連動
+// form.xに入力→mdl.mmbが更新
+// form2.submitを押すと、mld.mmbが100にセットされる、と同時に、form.xの値も100に更新
+
+
+
+
+var mdl = {
+    mmb: 1,
+    setter: (v) => mdl.mmb = v
+};
+
+var comp = {
+    view: function(ctrl){
+        return [
+            m("form#form", {
+                onsubmit: (e) => e.preventDefault()
+            }, [
+                m("label", "x"),
+                m("input[name=x][type=text]", {
+                    oninput: m.withAttr("value", mdl.setter),
+                    value: mdl.mmb
+                })
+            ]),
+            m("form#form2", {
+                onsubmit: (e) => {
+                    e.preventDefault();
+                    mdl.mmb = 100;
+                }
+            }, m("input[type=submit][value=送信]")
+             )
+        ];
+    }
+};
+
+m.mount(document.getElementById("root"), comp);
